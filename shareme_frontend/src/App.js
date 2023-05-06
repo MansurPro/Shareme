370a1e88c3a6ca90f010a44aed99980435904e96
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-06 10:21:33 -0500

    Social media frontend - starting to develop

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import Login from './components/Login';
-import Home from './container/Home';
+import Home from './container/Home';  
 
 const App = () => {
   return (
--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
   return (
