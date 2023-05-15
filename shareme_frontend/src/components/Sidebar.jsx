1f8d794448d537ab5f0f26f8e215c40f3abb323f
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-15 07:20:48 -0500

    Home page - sidebar implementing

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
 
-const Sidebar = () => {
+import logo from '../assets/logo.png';
+
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+
+const categories = [
+  { name: 'Animals'},
+  { name: 'Wallpaper'},
+  { name: 'Photography'},
+  { name: 'Gaming'},
+  { name: 'Coding'},
+  { name: 'Other'},
+]
+
+const Sidebar = ({ user, closeToggle }) => {
+
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  }
+
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className="flex flex-col">
+        <Link 
+          to='/'
+          className='flex px-5 gap-5 my-6 pt-1 w-190 items-center'
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt='logo' className='w-full'/>
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink 
+            to='/'
+            className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className='mt-2 px-5 text-base 2xl:text-xl'>
+            Discover categories
+          </h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+            >
+
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Router>
     <App />
   </Router>, 
-  document.getElementById('root')
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>, 
+//   document.getElementById('root')
+// );
