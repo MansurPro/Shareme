6db624c2dc3660dd2df0ddbe2edf518e2d9c8dbe
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-16 09:25:42 -0500

    Sidebar finished - Pins page starting

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -48,12 +48,27 @@ const Sidebar = ({ user, closeToggle }) => {
           {categories.slice(0, categories.length - 1).map((category) => (
             <NavLink
               to={`/category/${category.name}`}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSidebar}
+              key={category.name}
             >
-
+              {category.name}
             </NavLink>
           ))}
         </div>
       </div>
+
+      {user && (
+        <Link 
+          to={`user-profile/${user._id}`}
+          className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} className='w-10 h-10 rounded-full' alt='user-profile' />
+          <p>{user.userName}</p>
+          <IoIosArrowForward />
+        </Link>
+      )}
     </div>
   )
 }
--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-500'>
+        <Navbar 
+          searchTerm={searchTerm} 
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+    </div>
   )
 }
 
