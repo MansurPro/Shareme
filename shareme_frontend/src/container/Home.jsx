ce050284d97578ac89a9a8410a36cd2bd602317a
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-17 08:17:07 -0500

    Search bar implemented

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
             <img src={logo} alt='logo' className='w-28'/>
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt='user-profile' className='w-28' />
+            <img src={user?.image} alt='user-pic' className='w-28' />
           </Link>
         </div>
         {toggleSidebar && (
