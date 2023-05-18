097295f765220d9ec7bcd29a6c15ee64a63c9ab8
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-18 09:35:49 -0500

    Navigation bar - implemented

--- shareme_frontend/src/components/Navbar.jsx
+++ shareme_frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { IoMdAdd, IoMdSearch } from 'react-icons/io';
 
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
-
+  
   if (!user) return null;
 
   return (
@@ -21,12 +21,21 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
         />
       </div>
       <div className='flex gap-3'>
-        <Link to={`/`} >
-          <img 
-            src={user.image} 
+        <Link 
+          to={`user-profile/${user?._id}`}
+          className='hidden md:block'
+        >
+          <img
+            src={user?.image}
             alt='user-pic'
             className='w-14 h-12 rounded-lg'
           />
+        </Link>
+        <Link 
+          to="create-pin"
+          className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
+        >
+          <IoMdAdd />
         </Link>
       </div>
     </div>
