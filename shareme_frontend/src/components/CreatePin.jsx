e0b99a3cd70c000643d092fea92e955becaa2526
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-25 21:23:22 -0500

    Create pin - bug fixing

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -40,6 +40,39 @@ const CreatePin = ({ user }) => {
     }
   };
 
+  const savePin = () => {
+    if (title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          assets: {
+            _type: 'reference',
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id,
+        },
+        category,
+      }
+
+      client.create(doc)
+      .then(() => {
+        navigate('/');
+      });
+    } else {
+      setFields(true);
+
+      setTimeout(() => setFields(false), 2000)
+    }
+  };
+
   return (
     <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
       {fields && (
@@ -130,8 +163,26 @@ const CreatePin = ({ user }) => {
                 onChange={(e) => setCategory(e.target.value)}
                 className='outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'
               >
-                <option value='other' className='bg-white'>Select Category</option>
+                <option value='other' className='bg-white'>Select Category  </option>
+
+                {categories.map((category) => (
+                  <option 
+                    className='text-base border-0 outline-none capitalize bg-white text-black'
+                    value={category.name}
+                  >
+                    {category.name}
+                  </option>
+                ))}
               </select>
+            </div>
+            <div className='flex justify-end items-end mt-5'>
+              <button
+                type='button'
+                onClick={savePin}
+                className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'
+              >
+                Save Pin
+              </button>
             </div>
           </div>
         </div>
