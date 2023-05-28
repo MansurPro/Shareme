b179202805321d92a72ef2c1575f0b124af2527e
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-27 22:00:21 -0500

    Pin Detail - start implementing

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+import { client, urlFor } from '../utils/client.js';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+const PinDetail = ({ user }) => {
   return (
     <div>PinDetail</div>
   )
