d5a886f387ff56131c31ec584ce92d230ce1f566
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-05-29 19:56:40 -0500

    Pin Detail - going

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -63,6 +63,50 @@ const PinDetail = ({ user }) => {
               <MdDownloadForOffline />
             </a>
           </div>
+          <a
+            href={pinDetail.destination}
+            target='_blank'
+            rel='noreferrer'
+          >
+            {pinDetail.destination}
+          </a>
+        </div>
+        <div>
+          <h1 className='text-4xl font-bold break-words mt-3'>
+            {pinDetail.title}
+          </h1>
+          <p className='mt-3'>
+            {pinDetail.about}
+          </p>
+        </div>
+        <div>
+          <Link 
+            to={`/user-profile/${pinDetail?.postedBy._id}`} 
+            className="flex gap-2 mt-5 items-center bg-white rounded-lg "
+          >
+            <img 
+              src={pinDetail?.postedBy.image} 
+              className="w-10 h-10 rounded-full object-cover" 
+              alt="user-profile" 
+            />
+            <p className="font-semibold capitalize">{pinDetail?.postedBy.userName}</p>
+          </Link>
+          <h2 className='mt-5 text-2xl'>Comments</h2>
+          <div className='max-h-370 overflow-y-auto'>
+            {pinDetail?.comments?.map((comment, i) => (
+              <div className='flex gap-2 mt-5 items-center bg-white rounded-lg' key={i}>
+                <img 
+                  src={comment.postedBy.image}
+                  alt='user-profile'
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                />
+                <div className='flex flex-col'>
+                  <p className='font-bold'>{comment.postedBy.userName}</p>
+                  <p>{comment.comment}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
