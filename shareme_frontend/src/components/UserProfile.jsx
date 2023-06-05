f280e4d9aff20804f718777532b3edc95e34f4a6
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-06-04 19:29:37 -0500

    User Profile page - implementing

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { googleLogout } from '@react-oauth/google';
+
+import { userQuery, userCreatedPinsQuery, userSavedPinsQuery } from '../utils/data';
+import { client } from '../utils/client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query)
+    .then((data) => {
+      setUser(data[0]);
+    })
+  }, [userId]);
+
+  if (!user) {
+    return <Spinner message="Loading profile..."/>
+  }
+
   return (
-    <div>UserProfile</div>
+    <div className='relative pb-2 h-full justify-center items-center'>
+      <div className='flex flex-col pb-5'>
+        <div className='relative flex flex-col mb-7'>
+          <div className='flex flex-col justify-center items-center'>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
